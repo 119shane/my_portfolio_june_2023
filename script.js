466fd636e8541aa174e367cbde127d67445502b8
Shane Amal <21stwebwizard@gmail.com>
2023-06-20 18:18:43 +0530

    layout done
    navigation functioning and link other pages to be done

---
+++ script.js
@@ -0,0 +1,11 @@
+const backBtn = document.querySelector(".back_btn")
+const menuBtn = document.querySelector(".menu-btn")
+const leftCol = document.querySelector('.left_col')
+
+menuBtn.addEventListener('click', ()=> {
+    leftCol.style.transform = 'translateX(0)'
+})
+
+backBtn.addEventListener('click', ()=> {
+    leftCol.style.transform = 'translateX(-100%)'
+})
